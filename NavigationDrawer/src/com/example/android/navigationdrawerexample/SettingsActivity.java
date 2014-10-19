48e50a1028f3c5484defba0516b5a3ffddbab075
smallpotato99 <hacker1202@hotmail.com>
2014-10-19 16:39:10 +0800

    nothing changes

--- NavigationDrawer/src/com/example/android/navigationdrawerexample/SettingsActivity.java
+++ NavigationDrawer/src/com/example/android/navigationdrawerexample/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.android.navigationdrawerexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
 public class SettingsActivity extends Activity {
@@ -19,9 +20,11 @@ public class SettingsActivity extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		default:
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
 			return super.onOptionsItemSelected(item);
-		}		
 	}
 	
 }
